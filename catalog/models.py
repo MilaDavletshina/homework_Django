b7361c4731be193c2cce83f7aed2a001f6881577
ludmila davletshina <ludaplv@mail.ru>
2024-09-23 09:04:57 +0300

    requirements

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование продукта')
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='photos/', verbose_name='Изображение')
+    category = models.CharField(max_length=100, verbose_name='Категория', help_text='Введите наименование категории')
+    price = models.FloatField(verbose_name='Цена за покупку', help_text='Введите цену за покупку', null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name',]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Категория', help_text='Введите категорию продукта')
+    description = models.TextField(null=True, blank=True)
+    group = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='catalog')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name',]
+
