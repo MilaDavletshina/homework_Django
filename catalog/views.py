1cfd8807fd4199dcff85f9bfcb387358eb592fa7
ludmila davletshina <ludaplv@mail.ru>
2024-10-05 08:23:22 +0300

    добавлен шаблон product_form.html

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from catalog.models import Product
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
+
 
 def home(request):
     return render(request, 'home.html')
@@ -47,8 +48,8 @@ class ProductDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ('name', 'description', 'image', 'category', 'price', 'created_at', 'updated_at')
-    success_url = reverse_lazy('catalog:product_list')
+    fields = ('name', 'description', 'image', 'category', 'price')
+    success_url = reverse_lazy('catalog:products_list')
 
 
 class ProductUpdateView(UpdateView):
