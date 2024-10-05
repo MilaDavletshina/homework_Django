9c149e11926e2eb96928db0fdeb7873f91a41738
ludmila davletshina <ludaplv@mail.ru>
2024-10-05 10:55:26 +0300

    создан шаблон base

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from blogs.apps import BlogsConfig
-
+from blogs.views import base, PostListView
 
 
 app_name = BlogsConfig.name
 
 
-urlpatterns = []
+urlpatterns = [
+    path('', base, name='base'),
+    path('', PostListView.as_view(), name='post_list')
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from blogs.models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+
+class PostListView(ListView):
+    model = Post
