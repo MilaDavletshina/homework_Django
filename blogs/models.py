d970cc08a4b075a9e684446fade2292843092525
ludmila davletshina <ludaplv@mail.ru>
2024-10-05 10:04:36 +0300

    изменен файл models  для приложения blogs

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(null=True, blank=True)
+    preview = models.ImageField(upload_to='photos/blogs/', blank=True, null=True, verbose_name='Изображение')
+    created_at = models.DateField(auto_now_add=True)
+    is_published = models.BooleanField(default=False, verbose_name="Publication flag")
+    views_count = models.PositiveIntegerField(verbose_name='Количество просмотров', help_text='Укажите количество просмотров', default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        ordering = ['title',]
