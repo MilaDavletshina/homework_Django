263615c69474c33a87681c2754545e52ea9e3590
ludmila davletshina <ludaplv@mail.ru>
2024-10-23 14:44:20 +0300

    добавлена модель Users применены миграции

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, verbose_name='телефон', blank=True, null=True, help_text='Введите номер телефона') #можно подключить библиотеку телефона django phonenumber field
+    tg_name = models.CharField(max_length=50, verbose_name='ник телеграм', blank=True, null=True, help_text='Введите ник телеграм')
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='аватар', blank=True, null=True, help_text='Загрузите аватар')
+    token = models.CharField(max_length=100, verbose_name='token', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
