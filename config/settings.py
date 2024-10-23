01ffd507b3739b65411cc5bb506322cf70ddce8a
ludmila davletshina <ludaplv@mail.ru>
2024-10-23 14:35:13 +0300

    1) приложение users удалено 2) откат миграций auth 3) создано приложение users

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'blogs',
-    'users',
-    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
@@ -105,19 +103,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'user.User'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'drlelizaveta@gmail.com'
-EMAIL_HOST_PASSWORD = 'tnuz aszb ywad ghod'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-SERVER_EMAIL = EMAIL_HOST_USER
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls', namespace='catalog')),
-    path('blogs/', include('blogs.urls', namespace='blogs')),
-    path('users/', include ('users.urls', namespace='users'))
+    path('blogs/', include('blogs.urls', namespace='blogs'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
