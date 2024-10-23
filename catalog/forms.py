ab0df7411e70f252143854fe92c73b01feda30c0
ludmila davletshina <ludaplv@mail.ru>
2024-10-23 13:49:05 +0300

    добавлено новое приложение users

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.forms import ModelForm, BooleanField
-from django.shortcuts import redirect, render
+from django.forms import ModelForm
+
 
 from .models import Product
 
